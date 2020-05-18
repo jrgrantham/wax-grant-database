4695c904a2dda9c38d8e5c46658af85f4044e35b
James Grantham <james.r.grantham@gmail.com>
2020-05-18 23:31:27 +0100

    remove truncate from seeds

--- database/seeds/02_add_risks.js
+++ database/seeds/02_add_risks.js
@@ -1,82 +1,82 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("risks")
-    .truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex("risks").insert([
-        {
-          projectId: 1,
-          type: "managerial",
-          description: "managerial",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation1",
-        },
-        {
-          projectId: 1,
-          type: "commercial",
-          description: "commercial",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation2",
-        },
-        {
-          projectId: 1,
-          type: "commercial",
-          description: "commercial",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation3",
-        },
-        {
-          projectId: 1,
-          type: "commercial",
-          description: "commercial",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation4",
-        },
-        {
-          projectId: 1,
-          type: "commercial",
-          description: "commercial",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation5",
-        },
-        {
-          projectId: 1,
-          type: "technical",
-          description: "technical",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation6",
-        },
-        {
-          projectId: 1,
-          type: "environmental",
-          description: "environmental",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation7",
-        },
-        {
-          projectId: 1,
-          type: "legal",
-          description: "legal",
-          probability: 1,
-          consequence: 1,
-          owner: "JG",
-          mitigation: "mitigation8",
-        },
-      ]);
-    });
+  // return knex("risks")
+  //   .truncate()
+  //   .then(function () {
+  // Inserts seed entries
+  return knex("risks").insert([
+    {
+      projectId: 1,
+      type: "managerial",
+      description: "managerial",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation1",
+    },
+    {
+      projectId: 1,
+      type: "commercial",
+      description: "commercial",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation2",
+    },
+    {
+      projectId: 1,
+      type: "commercial",
+      description: "commercial",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation3",
+    },
+    {
+      projectId: 1,
+      type: "commercial",
+      description: "commercial",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation4",
+    },
+    {
+      projectId: 1,
+      type: "commercial",
+      description: "commercial",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation5",
+    },
+    {
+      projectId: 1,
+      type: "technical",
+      description: "technical",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation6",
+    },
+    {
+      projectId: 1,
+      type: "environmental",
+      description: "environmental",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation7",
+    },
+    {
+      projectId: 1,
+      type: "legal",
+      description: "legal",
+      probability: 1,
+      consequence: 1,
+      owner: "JG",
+      mitigation: "mitigation8",
+    },
+  ]);
+  // });
 };
--- database/seeds/03_add_templates.js
+++ database/seeds/03_add_templates.js
@@ -1,70 +1,70 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("templates")
-    .del()
-    .then(function () {
-      // Inserts seed entries
-      return knex("templates").insert([
-        {
-          type: "managerial",
-          description: "managerial description1",
-          probability: 1,
-          consequence: 1,
-          mitigation: "managerial mitigation1",
-          ai: true,
-          dlt: true,
-          man: true,
-        },
-        {
-          type: "commercial",
-          description: "commercial description2",
-          probability: 1,
-          consequence: 1,
-          mitigation: "commercial mitigation2",
-          ai: false,
-          dlt: true,
-          man: true,
-        },
-        {
-          type: "commercial",
-          description: "commercial description3",
-          probability: 1,
-          consequence: 1,
-          mitigation: "commercial mitigation3",
-          ai: true,
-          dlt: true,
-          man: true,
-        },
-        {
-          type: "technical",
-          description: "technical description4",
-          probability: 1,
-          consequence: 1,
-          mitigation: "technical mitigation4",
-          ai: true,
-          dlt: true,
-          man: true,
-        },
-        {
-          type: "environmental",
-          description: "environmental description5",
-          probability: 1,
-          consequence: 1,
-          mitigation: "environmental mitigation5",
-          ai: true,
-          dlt: true,
-          man: true,
-        },
-        {
-          type: "legal",
-          description: "legal description6",
-          probability: 1,
-          consequence: 1,
-          mitigation: "legal mitigation6",
-          ai: true,
-          dlt: true,
-          man: true,
-        },
-      ]);
-    });
+  // return knex("templates")
+  //   .del()
+  //   .then(function () {
+  // Inserts seed entries
+  return knex("templates").insert([
+    {
+      type: "managerial",
+      description: "managerial description1",
+      probability: 1,
+      consequence: 1,
+      mitigation: "managerial mitigation1",
+      ai: true,
+      dlt: true,
+      man: true,
+    },
+    {
+      type: "commercial",
+      description: "commercial description2",
+      probability: 1,
+      consequence: 1,
+      mitigation: "commercial mitigation2",
+      ai: false,
+      dlt: true,
+      man: true,
+    },
+    {
+      type: "commercial",
+      description: "commercial description3",
+      probability: 1,
+      consequence: 1,
+      mitigation: "commercial mitigation3",
+      ai: true,
+      dlt: true,
+      man: true,
+    },
+    {
+      type: "technical",
+      description: "technical description4",
+      probability: 1,
+      consequence: 1,
+      mitigation: "technical mitigation4",
+      ai: true,
+      dlt: true,
+      man: true,
+    },
+    {
+      type: "environmental",
+      description: "environmental description5",
+      probability: 1,
+      consequence: 1,
+      mitigation: "environmental mitigation5",
+      ai: true,
+      dlt: true,
+      man: true,
+    },
+    {
+      type: "legal",
+      description: "legal description6",
+      probability: 1,
+      consequence: 1,
+      mitigation: "legal mitigation6",
+      ai: true,
+      dlt: true,
+      man: true,
+    },
+  ]);
+  // });
 };
