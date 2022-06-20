8d0554aac8b9ae9128fc97beae7f8ee8d6491bc6
James Grantham <james.r.grantham@gmail.com>
2022-06-20 18:40:25 +0100

    revert cors

--- startup/routes.js
+++ startup/routes.js
@@ -30,12 +30,7 @@ module.exports = function (app) {
   //   origin: "*",
   //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   // };
-  app.use(
-    cors({
-      origin: "*",
-      credentials: true,
-    })
-  );
+  app.use(cors());
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
