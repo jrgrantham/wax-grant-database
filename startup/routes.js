d3d1a6f95992509a6927035f15a103f978d01cac
James Grantham <james.r.grantham@gmail.com>
2022-06-21 13:29:13 +0100

    header middleware to end

--- startup/routes.js
+++ startup/routes.js
@@ -30,25 +30,7 @@ module.exports = function (app) {
     origin: "*",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   };
-  app.use(cors(corsOptions));
-  app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+  app.use(cors());
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
@@ -75,4 +57,30 @@ module.exports = function (app) {
   app.use("/api/team", authenticate, team);
   app.use("/api/travel", authenticate, travel);
   app.use("/api/email", email);
+  
+  app.use(function (req, res, next) {
+    console.log("..... here .....");
+
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+  });
 };
